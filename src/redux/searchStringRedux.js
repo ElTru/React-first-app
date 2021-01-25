1d5b02f77426f9022722d141b5536e8d19d89342
ElTru <elzbieta.trusiewicz@gmail.com>
2021-01-25 15:28:03 +0100

    Add search string

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,32 @@
+//import shortid from 'shortid';
+
+// selectors
+export const getSearchString = ({searchString}) => searchString;
+
+export const countAllCards = ({cards}) => cards.length;
+
+export const countVisibleCards =({cards}, searchString) =>
+  cards.filter(card => new RegExp(searchString, 'i').test(card.title)).lenght;
+
+// action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// actions types
+export const CHANGE_SEARCHSTRING = createActionName('CHANGE_SEARCHSTRING');
+
+// action creators
+export const createAction_changeSearchString = payload => ({ //payload: { ...payload, id: shortid.generate() }, type: CHANGE_SEARCHSTRING })
+  payload,
+  type: CHANGE_SEARCHSTRING,
+});
+
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    case CHANGE_SEARCHSTRING:
+      return [action.payload];
+    default:
+      return statePart;
+  }
+}
